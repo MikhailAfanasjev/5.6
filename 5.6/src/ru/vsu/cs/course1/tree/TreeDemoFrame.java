59c5da6123a1eb54cee15588675becfce59cf215
MikhailAfanasjev <66779383+MikhailAfanasjev@users.noreply.github.com>
2020-07-05 18:48:35 +0300

    Add files via upload

---
+++ 5.6/src/ru/vsu/cs/course1/tree/TreeDemoFrame.java
@@ -0,0 +1,222 @@
+package ru.vsu.cs.course1.tree.demo;
+
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
+import com.intellij.uiDesigner.core.Spacer;
+import ru.vsu.cs.course1.tree.BinaryTree;
+import ru.vsu.cs.course1.tree.BinaryTreePainter;
+import ru.vsu.cs.course1.tree.DefaultBinaryTree;
+import ru.vsu.cs.course1.tree.bst.DefaultBSTree;
+import ru.vsu.cs.course1.tree.bst.SimpleBSTree;
+import ru.vsu.cs.course1.tree.bst.SimpleBSTreeMap;
+import ru.vsu.cs.course1.tree.bst.avl.AVLTreeMap;
+import ru.vsu.cs.course1.tree.bst.rb.RBTree;
+import ru.vsu.cs.course1.tree.bst.rb.RBTreeMap;
+import ru.vsu.cs.util.ArrayUtils;
+import ru.vsu.cs.util.SwingUtils;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Map;
+
+public class TreeDemoFrame extends JFrame {
+    private JPanel panelMain;
+    private JButton buttonPreOrderTraverse;
+    private JTextArea textAreaSystemOut;
+    private JTextField textFieldBracketNotationTree;
+    private JButton buttonMakeTree;
+    private JSplitPane splitPaneMain;
+    private JTextField textFieldSingleValue;
+    private JPanel panelPaintArea;
+
+    private JMenuBar menuBarMain;
+    private JPanel paintPanel = null;
+    private JFileChooser fileChooserSave = null;
+
+    DefaultBinaryTree<Double> tree = new BinaryTree<>();
+
+
+    public TreeDemoFrame() {
+        this.setTitle("Двоичные деревья");
+        this.setContentPane(panelMain);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+
+        createMenu();
+
+        splitPaneMain.setDividerLocation(0.5);
+        splitPaneMain.setResizeWeight(1.0);
+        splitPaneMain.setBorder(null);
+
+        paintPanel = new JPanel() {
+            @Override
+            public void paintComponent(Graphics gr) {
+                super.paintComponent(gr);
+                Point size = BinaryTreePainter.paint(tree, gr);
+                SwingUtils.setFixedSize(paintPanel, size.x, size.y);
+            }
+        };
+        JScrollPane paintJScrollPane = new JScrollPane(paintPanel);
+        panelPaintArea.add(paintJScrollPane);
+
+        fileChooserSave = new JFileChooser();
+        fileChooserSave.setCurrentDirectory(new File("./images"));
+        FileFilter filter = new FileNameExtensionFilter("SVG images", "svg");
+        fileChooserSave.addChoosableFileFilter(filter);
+        fileChooserSave.setAcceptAllFileFilterUsed(false);
+        fileChooserSave.setDialogType(JFileChooser.SAVE_DIALOG);
+        fileChooserSave.setApproveButtonText("Save");
+
+        buttonMakeTree.addActionListener(actionEvent -> {
+            try {
+                BinaryTree<Double> tree = new BinaryTree<>(s -> Double.parseDouble(s));
+                tree.fromBracketNotation(textFieldBracketNotationTree.getText());
+                this.tree = tree;
+                repaintTree();
+            } catch (Exception ex) {
+                SwingUtils.showErrorMessageBox(ex);
+            }
+        });
+
+        buttonPreOrderTraverse.addActionListener(actionEvent -> {
+            showSystemOut(() -> {
+                System.out.println("Посетитель:");
+                tree.preOrderVisit((value, level) -> {
+                    System.out.println(value + " (уровень " + level + ")");
+                });
+                System.out.println();
+                System.out.println("Итератор:");
+                double min = 100000;
+                for (Double i : tree.preOrderValues()) {
+                    if (i < min)
+                        min = i;
+                    System.out.println(i);
+                }
+                System.out.println("Минимум " + min);
+            });
+        });
+    }
+
+    /**
+     * Создание меню
+     */
+    private void createMenu() {
+        JMenu menuTesting = new JMenu("Тестирование");
+        Class[] mapClasses = {SimpleBSTreeMap.class, AVLTreeMap.class, RBTreeMap.class};
+        for (Class mapClass : mapClasses) {
+            JMenuItem menuItem = new JMenuItem("Корректность " + mapClass.getSimpleName());
+            menuItem.addActionListener(actionEvent -> {
+                try {
+                    Map<Integer, Integer> map = (Map<Integer, Integer>) mapClass.getConstructor().newInstance();
+                    showSystemOut(() -> {
+                        MapTest.testCorrect(map);
+                    });
+                } catch (Exception e) {
+                    SwingUtils.showErrorMessageBox(e);
+                }
+            });
+            menuTesting.add(menuItem);
+        }
+
+        menuBarMain = new JMenuBar();
+        menuBarMain.add(menuTesting);
+        setJMenuBar(menuBarMain);
+    }
+
+    /**
+     * Перерисовка дерева
+     */
+    public void repaintTree() {
+        paintPanel.repaint();
+    }
+
+    /**
+     * Выполнение действия с выводом стандартного вывода в окне (textAreaSystemOut)
+     *
+     * @param action Выполняемое действие
+     */
+    private void showSystemOut(Runnable action) {
+        PrintStream oldOut = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            System.setOut(new PrintStream(baos, true, "UTF-8"));
+
+            action.run();
+
+            textAreaSystemOut.setText(baos.toString("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            SwingUtils.showErrorMessageBox(e);
+        }
+        System.setOut(oldOut);
+    }
+
+    /**
+     * Заполнить дерево добавлением всех элементов (textFieldValues)
+     *
+     * @param tree Дерево
+     */
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        panelMain = new JPanel();
+        panelMain.setLayout(new GridLayoutManager(1, 1, new Insets(10, 10, 10, 10), 10, 10));
+        splitPaneMain = new JSplitPane();
+        panelMain.add(splitPaneMain, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(200, 200), null, 0, false));
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        splitPaneMain.setLeftComponent(panel1);
+        final JPanel panel2 = new JPanel();
+        panel2.setLayout(new GridLayoutManager(3, 3, new Insets(0, 0, 0, 0), -1, -1));
+        panel1.add(panel2, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("Дерево в скобочной нотации:");
+        panel2.add(label1, new GridConstraints(0, 0, 1, 3, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        textFieldBracketNotationTree = new JTextField();
+        textFieldBracketNotationTree.setText("8 (6 (4 (5), 6), 5 (, 5 (2, 8)))");
+        panel2.add(textFieldBracketNotationTree, new GridConstraints(1, 0, 1, 3, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
+        buttonMakeTree = new JButton();
+        buttonMakeTree.setText("Построить дерево");
+        panel2.add(buttonMakeTree, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final Spacer spacer1 = new Spacer();
+        panel2.add(spacer1, new GridConstraints(2, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
+        panelPaintArea = new JPanel();
+        panelPaintArea.setLayout(new BorderLayout(0, 0));
+        panel1.add(panelPaintArea, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        final Spacer spacer2 = new Spacer();
+        panelPaintArea.add(spacer2, BorderLayout.CENTER);
+        final JPanel panel3 = new JPanel();
+        panel3.setLayout(new GridLayoutManager(2, 1, new Insets(0, 0, 0, 0), -1, -1));
+        splitPaneMain.setRightComponent(panel3);
+        buttonPreOrderTraverse = new JButton();
+        buttonPreOrderTraverse.setText("Прямой обход");
+        panel3.add(buttonPreOrderTraverse, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JScrollPane scrollPane1 = new JScrollPane();
+        panel3.add(scrollPane1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        textAreaSystemOut = new JTextArea();
+        scrollPane1.setViewportView(textAreaSystemOut);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return panelMain;
+    }
+
+}
